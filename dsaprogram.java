424f06c3a9f0a8a1700d751fbb72e167f6dc0501
sowndarya9632 <sowndarya9632@gmail.com>
2024-09-30 12:09:28 +0530

    add functionality to checklastfirstoccurence

--- dsaprogram.java
+++ dsaprogram.java
@@ -1,9 +1,9 @@
 package binarysearch;
 
-public class BinarySearchBoundary {
+public class BinarySearchOccurrences {
 
-    // Method to find the smallest element greater than or equal to target
-    public static int findSmallestGreaterThanOrEqual(int[] arr, int target) {
+    // Method to find the first occurrence of a target element
+    public static int findFirstOccurrence(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
         int result = -1;
@@ -11,19 +11,21 @@ public class BinarySearchBoundary {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (arr[mid] >= target) {
+            if (arr[mid] == target) {
                 result = mid; // Update result and continue searching left
                 right = mid - 1;
-            } else {
+            } else if (arr[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return result; // Returns -1 if not found
+        return result; // Will return -1 if not found
     }
 
-    // Method to find the largest element smaller than or equal to target
-    public static int findLargestSmallerThanOrEqual(int[] arr, int target) {
+    // Method to find the last occurrence of a target element
+    public static int findLastOccurrence(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
         int result = -1;
@@ -31,27 +33,27 @@ public class BinarySearchBoundary {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (arr[mid] <= target) {
+            if (arr[mid] == target) {
                 result = mid; // Update result and continue searching right
+                left = mid + 1;
+            } else if (arr[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
 
-        return result; // Returns -1 if not found
+        return result; // Will return -1 if not found
     }
 
     public static void main(String[] args) {
-        int[] sortedArray = {1, 2, 4, 5, 6, 8, 10};
-        int targetValue1 = 5;
-        int targetValue2 = 7;
+        int[] sortedArray = {1, 2, 2, 2, 3, 4, 5, 5, 5, 6};
+        int targetValue = 5;
 
-        int index1 = findSmallestGreaterThanOrEqual(sortedArray, targetValue1);
-        int index2 = findLargestSmallerThanOrEqual(sortedArray, targetValue2);
+        int firstIndex = findFirstOccurrence(sortedArray, targetValue);
+        int lastIndex = findLastOccurrence(sortedArray, targetValue);
 
-        System.out.println("Index of smallest element >= " + targetValue1 + ": " + index1); // Output: 3
-        System.out.println("Index of largest element <= " + targetValue2 + ": " + index2); // Output: 4
+        System.out.println("First occurrence of " + targetValue + ": " + firstIndex); // Output: 6
+        System.out.println("Last occurrence of " + targetValue + ": " + lastIndex);   // Output: 8
     }
 }
-
